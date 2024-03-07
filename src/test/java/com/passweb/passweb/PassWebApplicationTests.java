9178f16991de402bfab9f7f0a9a79dc18660cc16
kimseongsu <ss.kim@okestro.com>
2024-03-07 14:50:42 +0900

    init commit

---
+++ src/test/java/com/passweb/passweb/PassWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passweb.passweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
