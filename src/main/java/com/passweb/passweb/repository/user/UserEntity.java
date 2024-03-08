ba8141637a6fac95e82ee2613ca97d75557c631c
kimseongsu <ss.kim@okestro.com>
2024-03-08 14:23:21 +0900

    [feature] user entity, enum, repository add
    - user entity, enum, repository add

---
+++ src/main/java/com/passweb/passweb/repository/user/UserEntity.java
@@ -0,0 +1,26 @@
+package com.passweb.passweb.repository.user;
+
+import com.passweb.passweb.repository.BaseEntity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "user")
+public class UserEntity extends BaseEntity {
+    @Id
+    private String userId;
+
+    private String userName;
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;
+    private String phone;
+
+    private String meta;
+
+}
---
+++ src/main/java/com/passweb/passweb/repository/user/UserStatus.java
@@ -0,0 +1,5 @@
+package com.passweb.passweb.repository.user;
+
+public enum UserStatus {
+    ACTIVE, INACTIVE
+}
