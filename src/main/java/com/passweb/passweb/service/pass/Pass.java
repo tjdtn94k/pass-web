702e413f446bda9f7fe50ef9c4c1645e2fdffb92
kimseongsu <ss.kim@okestro.com>
2024-03-13 16:58:28 +0900

    [feature] 이용권 service 추가 - 이용권 service 추가

---
+++ src/main/java/com/passweb/passweb/service/pass/Pass.java
@@ -0,0 +1,26 @@
+package com.passweb.passweb.service.pass;
+
+import com.passweb.passweb.repository.pass.PassStatus;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+public class Pass {
+    private Integer passSeq;
+    private Integer packageSeq;
+    private String packageName;
+    private String userId;
+
+    private PassStatus status;
+    private Integer remainingCount;
+
+    private LocalDateTime startedAt;
+    private LocalDateTime endedAt;
+    private LocalDateTime expiredAt;
+
+}
---
+++ src/main/java/com/passweb/passweb/service/pass/PassService.java
@@ -0,0 +1,23 @@
+package com.passweb.passweb.service.pass;
+
+import com.passweb.passweb.repository.pass.PassEntity;
+import com.passweb.passweb.repository.pass.PassRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PassService {
+    private final PassRepository passRepository;
+
+    public PassService(PassRepository passRepository) {
+        this.passRepository = passRepository;
+    }
+
+    public List<Pass> getPasses(final String userId) {
+        final List<PassEntity> passEntities = passRepository.findByUserId(userId);
+        return PassModelMapper.INSTANCE.map(passEntities);
+
+    }
+
+}
