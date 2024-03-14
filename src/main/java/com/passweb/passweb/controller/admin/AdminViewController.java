cb1da54a4805b260ec11e35c8637fd2533b64973
kimseongsu <ss.kim@okestro.com>
2024-03-14 09:57:37 +0900

    [feature] 각 Service 별 Controller add - 각 Service 별 Controller add

---
+++ src/main/java/com/passweb/passweb/controller/admin/AdminViewController.java
@@ -0,0 +1,53 @@
+package com.passweb.passweb.controller.admin;
+
+import com.passweb.passweb.service.packaze.PackageService;
+import com.passweb.passweb.service.pass.BulkPassService;
+import com.passweb.passweb.service.statistics.StatisticsService;
+import com.passweb.passweb.service.user.UserGroupMappingService;
+import com.passweb.passweb.util.LocalDateTimeUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDateTime;
+
+@Controller
+@RequestMapping(value = "/admin")
+public class AdminViewController {
+    @Autowired
+    private BulkPassService bulkPassService;
+    @Autowired
+    private PackageService packageService;
+    @Autowired
+    private UserGroupMappingService userGroupMappingService;
+    @Autowired
+    private StatisticsService statisticsService;
+
+    @GetMapping
+    public ModelAndView home(ModelAndView modelAndView, @RequestParam("to") String toString) {
+        LocalDateTime to = LocalDateTimeUtils.parseDate(toString);
+
+        modelAndView.addObject("chartData", statisticsService.makeChartData(to));
+        modelAndView.setViewName("admin/index");
+        return modelAndView;
+    }
+
+    @GetMapping("/bulk-pass")
+    public ModelAndView registerBulkPass(ModelAndView modelAndView) {
+        modelAndView.addObject("bulkPasses", bulkPassService.getAllBulkPasses());
+        modelAndView.addObject("packages", packageService.getAllPackages());
+        modelAndView.addObject("userGroupIds", userGroupMappingService.getAllUserGroupIds());
+        modelAndView.addObject("request", new BulkPassRequest());
+        modelAndView.setViewName("admin/bulk-pass");
+
+        return modelAndView;
+    }
+
+    @PostMapping("/bulk-pass")
+    public String addBulkPass(@ModelAttribute("request") BulkPassRequest request, Model model) {
+        bulkPassService.addBulkPass(request);
+        return "redirect:/admin/bulk-pass";
+    }
+}
---
+++ src/main/java/com/passweb/passweb/controller/admin/BulkPassRequest.java
@@ -0,0 +1,23 @@
+package com.passweb.passweb.controller.admin;
+
+import com.passweb.passweb.util.LocalDateTimeUtils;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+public class BulkPassRequest {
+    private Integer packageSeq;
+    private String userGroupId;
+    private LocalDateTime startedAt;
+
+    public void setStartedAt(String startedAtString) {
+        this.startedAt = LocalDateTimeUtils.parse(startedAtString);
+
+    }
+
+}
